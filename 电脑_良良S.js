9b73229a1408ad31d91451b98839c404b29b0dc3
Lemon399 <1446744399@qq.com>
2021-01-05 16:48:37 +0800

    multiBase test

---
+++ 电脑_良良S.js
@@ -0,0 +1,55 @@
+加入("标签",{
+	pc: ["电脑","darkblue","white"],
+    net: [".NET","white","purple"],
+});
+加入("APP",{
+dingpc: {
+	名称: "钉钉",
+	前置: "不是",
+	无缝: "不是",
+	标签: ["阿里","电脑"],
+	内容:" Chrome/63.0.3239.132 Safari/537.36 dingtalk-win/1.0.0 nw(0.14.7) DingTalk(5.0.0-Release.74) Mojo/1.0.0 Native AppType(release)"
+},
+"360somgrpc": {
+	名称: "360 软件管家",
+	前置: "不是",
+	无缝: "不是",
+	标签: ["360","电脑",".NET"],
+	内容:" 360SoftMgr)"
+},
+wxclientpc: {
+	名称: "微信",
+	前置: "不是",
+	无缝: "不是",
+	标签: ["腾讯","电脑"],
+	内容:" QBCore/4.0.1295.400 QQBrowser/9.0.2524.400 NetType/WIFI MicroMessenger/7.0.5 WindowsWechat"
+},
+qqapc: {
+	名称: "QQ",
+	前置: "不是",
+	无缝: "不是",
+	标签: ["腾讯","电脑"],
+	内容:" QQ/9.2.2.26569 Chrome/43.0.2357.134 Safari/537.36 QBCore/3.43.1284.400 QQBrowser/9.0.2524.400"
+},
+zmbdpc: {
+	名称: "桌面百度",
+	前置: "不是",
+	无缝: "不是",
+	标签: ["百度","电脑",".NET"],
+	内容:" BaiduClient/2.2.0.1452)"
+},
+timpc: {
+	名称: "TIM",
+	前置: "不是",
+	无缝: "不是",
+	标签: ["腾讯","电脑"],
+	内容:" QQ/3.0.0.21302 TIM2.0/3.0.0.21302 Chrome/43.0.2357.134 Safari/537.36 QBCore/3.43.1298.400 QQBrowser/9.0.2524.400"
+},
+wxxCx: {
+	名称: "微信小程序",
+	前置: "不是",
+	无缝: "不是",
+	标签: ["腾讯","电脑"],
+	内容:" Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat"
+},
+});
