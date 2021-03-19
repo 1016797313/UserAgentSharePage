453fb1bab6d8f4cc9ee3fac0f884c7773d699612
Lemon399 <1446744399@qq.com>
2021-03-19 15:44:54 +0800

    5.1 Beta 1: 夜间主题...

--- svcWorker.js
+++ svcWorker.js
@@ -1,5 +1,5 @@
 var data = [
-  ["I","./index.html",21031915],
+  ["I","./index.html",21031916],
   ["L","./数据列表.js",2],
   ["B","./基础_良良.js",21031013],
   ["A","./UA_良良.js",21031416],
@@ -8,7 +8,7 @@ var data = [
   ["S","./爬虫_良良.js",21031415],
   ["T","./indexSidebar.js",2],
   ["O","../favicon.ico",2],
-  ["D","./",21031915],
+  ["D","./",21031916],
 ];
 
 self.addEventListener('install', function(event) {
