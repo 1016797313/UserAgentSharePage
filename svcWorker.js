13b168bed434151680dbe561cc277f5a6f07b1a8
lemon399 <1446744399@qq.com>
2021-05-02 14:51:29 +0800

    打开窗口的按钮也可以关闭窗口，生成按钮除外

--- svcWorker.js
+++ svcWorker.js
@@ -1,5 +1,5 @@
 var data = [
-  ["I","./index.html",21050213],
+  ["I","./index.html",21050214],
   ["L","./数据列表.js",2],
   ["B","./基础_良良.js",21040914],
   ["A","./UA_良良.js",21031416],
@@ -8,7 +8,7 @@ var data = [
   ["S","./爬虫_良良.js",21031415],
   ["T","./indexSidebar.js",2],
   ["O","../favicon.ico",2],
-  ["D","./",21050213],
+  ["D","./",21050214],
 ];
 
 self.addEventListener('install', function(event) {
